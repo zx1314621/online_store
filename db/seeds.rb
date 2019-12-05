ed65667ebaf9322fe43aeb09c872eb10d0288f1c
zx1314621 <935638374@qq.com>
2019-12-04 21:48:36 -0800

    first version

---
+++ db/seeds.rb
@@ -0,0 +1,42 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: 'When We Believed in Mermaids', description: 'Her sister has been dead for fifteen years when she sees her on the TV news…
+
+Josie Bianci was killed years ago on a train during a terrorist attack. Gone forever. It’s what her sister, Kit, an ER doctor in Santa Cruz, has always believed. Yet all it takes is a few heart-wrenching seconds to upend Kit’s world. Live coverage of a club fire in Auckland has captured the image of a woman stumbling through the smoke and debris. Her resemblance to Josie is unbelievable. And unmistakable. With it comes a flood of emotions—grief, loss, and anger—that Kit finally has a chance to put to rest: by finding the sister who’s been living a lie.
+
+After arriving in New Zealand, Kit begins her journey with the memories of the past: of days spent on the beach with Josie. Of a lost teenage boy who’d become part of their family. And of a trauma that has haunted Kit and Josie their entire lives.
+
+Now, if two sisters are to reunite, it can only be by unearthing long-buried secrets and facing a devastating truth that has kept them apart far too long. To regain their relationship, they may have to lose everything.', image: './images/Mermaids.jpg', price: '6.34')
+
+Product.create(name: 'Butterfly in Frost', description: 'Once, I would never have imagined myself here. But I’m settled now. In a place I love, in a home I renovated, spending time with new friends I adore, and working a job that fulfills me. I am reconciling the past and laying the groundwork for the future.
+
+Then Garrett Frost moves in next door.
+
+He’s obstinate and too bold, a raging force of nature that disrupts the careful order of my life. I recognize the ghosts that haunt him, the torment driving him. Garrett would be risky in any form, but wounded, he’s far more dangerous. I fear I’m too fragile for the storm raging inside him, too delicate to withstand the pain that buffets him. But he’s too determined…and too tempting.
+
+And sometimes hope soars above even the iciest desolation.', image: './images/Butterfly in Frost.jpg', price: '5.00')
+Product.create(name: 'Where the Forest Meets the Stars', description: 'After the loss of her mother and her own battle with breast cancer, Joanna Teale returns to her graduate research on nesting birds in rural Illinois, determined to prove that her recent hardships have not broken her. She throws herself into her work from dusk to dawn, until her solitary routine is disrupted by the appearance of a mysterious child who shows up at her cabin barefoot and covered in bruises.
+
+The girl calls herself Ursa, and she claims to have been sent from the stars to witness five miracles. With concerns about the child’s home situation, Jo reluctantly agrees to let her stay—just until she learns more about Ursa’s past.
+
+Jo enlists the help of her reclusive neighbor, Gabriel Nash, to solve the mystery of the charming child. But the more time they spend together, the more questions they have. How does a young girl not only read but understand Shakespeare? Why do good things keep happening in her presence? And why aren’t Jo and Gabe checking the missing children’s website anymore?
+
+Though the three have formed an incredible bond, they know difficult choices must be made. As the summer nears an end and Ursa gets closer to her fifth miracle, her dangerous past closes in. When it finally catches up to them, all of their painful secrets will be forced into the open, and their fates will be left to the stars.', image: './images/Where the Forest Meets the Stars.jpg', price: '7.48')
+Product.create(name: 'Thin Air', description: 'Private investigator Jessica Shaw is used to getting anonymous tips. But after receiving a photo of a three-year-old kidnapped from Los Angeles twenty-five years ago, Jessica is stunned to recognize the little girl as herself.
+
+Eager for answers, Jessica heads to LA’s dark underbelly. When she learns that her biological mother was killed the night she was abducted, Jessica’s determined to solve a case the police have forgotten. Meanwhile, veteran LAPD detective Jason Pryce is in the midst of a gruesome investigation into a murdered college student moonlighting as a prostitute. A chance encounter leads to them crossing paths, but Jessica soon realizes that Pryce is hiding something about her father’s checkered history and her mother’s death.
+
+To solve her mother’s murder and her own disappearance, Jessica must dig into the past and find the secrets buried there. But the air gets thinner as she crawls closer to the truth, and it’s getting harder and harder to breathe.', image: './images/Thin Air.jpg', price: '6.38')
+Product.create(name: 'The Beantown Girls', description: '1944: Fiona Denning has her entire future planned out. She’ll work in city hall, marry her fiancé when he returns from the war, and settle down in the Boston suburbs. But when her fiancé is reported missing after being shot down in Germany, Fiona’s long-held plans are shattered.
+
+Determined to learn her fiancé’s fate, Fiona leaves Boston to volunteer overseas as a Red Cross Clubmobile girl, recruiting her two best friends to come along. There’s the outspoken Viviana, who is more than happy to quit her secretarial job for a taste of adventure. Then there’s Dottie, a shy music teacher whose melodious talents are sure to bring heart and hope to the boys on the front lines.
+
+Chosen for their inner strength and outer charm, the trio isn’t prepared for the daunting challenges of war. But through it all come new friendships and romances, unforeseen dangers, and unexpected dreams. As the three friends begin to understand the real reasons they all came to the front, their courage and camaraderie will see them through some of the best and worst times of their lives.', image: './images/The Beantown Girls.jpg', price: '6.99')
+Product.create(name: 'White Rose, Black Forest', description: 'December 1943. In the years before the rise of Hitler, the Gerber family’s summer cottage was filled with laughter. Now, as deep drifts of snow blanket the Black Forest, German dissenter Franka Gerber is alone and hopeless. Fervor and brutality have swept through her homeland, taking away both her father and her brother and leaving her with no reason to live.
+
+That is, until she discovers an unconscious airman lying in the snow wearing a Luftwaffe uniform, his parachute flapping in the wind. Unwilling to let him die, Franka takes him to her family’s isolated cabin despite her hatred for the regime he represents. But when it turns out that he is not who he seems, Franka begins a race against time to unravel the mystery of the airman’s true identity. Their tenuous bond becomes as inseparable as it is dangerous. Hunted by the Gestapo, can they trust each other enough to join forces on a mission that could change the face of the war and their own lives forever?', image: './images/White Rose, Black Forest.jpg', price: '7.48')
